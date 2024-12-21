de71c6d85af1afba8ef8a93b7912bd61d10d4f2f
Tanmyname <tanxzxzxzxz@gmail.com>
2024-12-21 17:15:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "system :"; pkg install ffmpeg
 echo "system :"; pkg install nodejs
-echo "system :"; pkg install git
-echo "system :"; libwebp
-echo "system :"; pkg install python
+echo "system :"; pkg install libwebp
 echo "system :"; pkg install yarn 
 echo "system :"; pkg install yarn install
 
